057a26b5475e55bb6e6361b199bebb5cf5651348
AlexLloret <alexlloretrocher@gmail.com>
2024-12-09 19:16:03 +0100

    JS Revisado

---
+++ docs/LMI/js/ACTIVITAT10.js
@@ -0,0 +1,13 @@
+// Bucle while
+let i = 10;
+while (i >= 0) {
+  console.log(i);
+  i--;  // Restem 1 a i en cada iteració
+}
+
+// Bucle do...while
+let j = 10;
+do {
+  console.log(j);
+  j--;  // Restem 1 a j en cada iteració
+} while (j >= 0);
