8512951794cb852f01b2d506b16ebb938a114bd3
AlexLloret <alexlloretrocher@gmail.com>
2024-12-09 18:21:57 +0100

    PRUEBA JS

---
+++ md_docs/LMI/js/ACTIVITAT1.js
@@ -0,0 +1,15 @@
+// Declaració de les tres variables amb els colors preferits
+let color1 = "Blau";
+let color2 = "Verd";
+let color3 = "Groc";
+
+// Mostrem les variables a la consola
+console.log(color1);
+console.log(color2);
+console.log(color3);
+
+// Combina les tres variables en una nova variable
+let combinacioColors = color1 + " " + color2 + " " + color3;
+
+// Mostrem el resultat de la combinació
+console.log(combinacioColors);
